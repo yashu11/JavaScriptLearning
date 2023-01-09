50e28b1090dc277117790b6059ae104d41490870
unknown <yashika.ait@gmail.com>
2023-01-09 21:08:48 +0000

    ObjectDestructring

---
+++ ObjectDestructuring.js
@@ -0,0 +1,11 @@
+var Hero = {
+  name: "Batman",
+  realName: "Bruce Waye",
+};
+
+// var name = Hero.name;
+// var realName = Hero.realName;
+
+const { name, realName } = Hero;
+
+console.log(name + "" + realName);
